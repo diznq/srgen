f8a8dead6454c8c2021964a6f5c06672d956cf3c
diznq <13458283+diznq@users.noreply.github.com>
2023-02-04 19:05:27 +0100

    feat: cleanup code

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 gcc src/SR.c \
-    -DTRANSFORM="${TRANSFORM:-transform_rgb3}" \
+    -DTRANSFORM="${TRANSFORM:-transform_yuv3}" \
     -DTRANSFORM_SIZE="${TRANSFORM_SIZE:-3}" \
-    -DBLOCK_SIZE_LOG="${BLOCK_SIZE_LOG:-3}" \
+    -DBLOCK_SIZE_LOG="${BLOCK_SIZE_LOG:-4}" \
     -DTRANSFORMS="${TRANSFORMS:-6}" \
-    -DSIMILARITY="${SIMILARITY:-COS}" \
+    -DSIMILARITY="${SIMILARITY:-NORMAL}" \
     -funroll-all-loops \
     -Ofast -march=native -mtune=intel -o "${OUTPUT:-bin/SR-gcc.exe}"
